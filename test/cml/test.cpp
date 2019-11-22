181ddfc59aa330e600a24f85159a08bca98b28e0
LazyMechanic <AsharnRus@gmail.com>
2019-11-22 23:59:54 +0300

    Update project structure

---
+++ test/cml/test.cpp
@@ -0,0 +1,40 @@
+#include <array>
+#include <chrono>
+#include <cml/cml.hh>
+#include <fbi/fbi.hh>
+#include <iostream>
+#include <numeric>
+
+int main()
+{
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    std::chrono::time_point<std::chrono::high_resolution_clock> stop;
+
+    constexpr std::int32_t testRepeatCount = 1000;
+    constexpr std::uint32_t primeDimension = 64;
+    using Timeholder                       = double;
+
+    std::array<Timeholder, testRepeatCount> testDurations{};
+    mech::crypt::MilRabPrimeGenerator<primeDimension, fbi::BigUnsigned> primeGenerator;
+
+    for (std::int32_t i = 0; i < testRepeatCount; ++i) {
+        start = std::chrono::high_resolution_clock::now();
+
+        auto prime = primeGenerator();
+        (void)(prime);
+
+        stop = std::chrono::high_resolution_clock::now();
+
+        testDurations[i] = std::chrono::duration<Timeholder, std::milli>(stop - start).count();
+    }
+
+    auto averageDuration =
+        std::accumulate(testDurations.begin(), testDurations.end(), 0) / static_cast<Timeholder>(testRepeatCount);
+
+    std::cout << "Test repeat count:            " << testRepeatCount << std::endl
+              << "Prime number dimension (bits) " << primeDimension << std::endl
+              << "Average generation time (ms): " << averageDuration << std::endl;
+
+    /* nothing to do */
+    return 0;
+}
